1f9924ec8f40aae75d519fb1df7a125aad252442
Rodrigo Ribeiro <jr.rodrigo.pt@gmail.com>
2021-04-12 10:03:26 +0100

    aula 3 e 2 users server discovery

---
+++ src/tp1/discovery/Discovery.java
@@ -0,0 +1,167 @@
+package tp1.discovery;
+
+import java.io.IOException;
+import java.net.*;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+import java.util.logging.Logger;
+
+/**
+ * <p>A class to perform service discovery, based on periodic service contact endpoint 
+ * announcements over multicast communication.</p>
+ * 
+ * <p>Servers announce their *name* and contact *uri* at regular intervals. The server actively
+ * collects received announcements.</p>
+ * 
+ * <p>Service announcements have the following format:</p>
+ * 
+ * <p>&lt;service-name-string&gt;&lt;delimiter-char&gt;&lt;service-uri-string&gt;</p>
+ */
+public class Discovery {
+	private static Logger Log = Logger.getLogger(Discovery.class.getName());
+
+	static {
+		// addresses some multicast issues on some TCP/IP stacks
+		System.setProperty("java.net.preferIPv4Stack", "true");
+		// summarizes the logging format
+		System.setProperty("java.util.logging.SimpleFormatter.format", "%4$s: %5$s");
+	}
+	
+	
+	// The pre-aggreed multicast endpoint assigned to perform discovery. 
+	static final InetSocketAddress DISCOVERY_ADDR = new InetSocketAddress("226.226.226.226", 2266);
+	static final int DISCOVERY_PERIOD = 1000;
+	static final int DISCOVERY_TIMEOUT = 5000;
+
+	// Used separate the two fields that make up a service announcement.
+	private static final String DELIMITER = "\t";
+
+	private InetSocketAddress addr;
+	private String serviceName;
+	private String serviceURI;
+	private Map<String, Set<URI>> servers;
+	private Map<String, Long> timeStamps;
+	private MulticastSocket ms;
+
+	/**
+	 * @param  serviceName the name of the service to announce
+	 * @param  serviceURI an uri string - representing the contact endpoint of the service being announced
+	 */
+	public Discovery(InetSocketAddress addr, String serviceName, String serviceURI) {
+		this.addr = addr;
+		this.serviceName = serviceName;
+		this.serviceURI  = serviceURI;
+		this.servers = new HashMap<String, Set<URI>>();
+		this.timeStamps = new HashMap<String, Long>();
+		this.ms = null;
+	}
+
+	/**
+	 * @param  serviceName the name of the service to announce
+	 * @param  serviceURI an uri string - representing the contact endpoint of the service being announced
+	 */
+	public Discovery(String serviceName, String serviceURI) {
+		this(DISCOVERY_ADDR, serviceName, serviceURI);
+	}
+	
+	/**
+	 * Starts sending service announcements at regular intervals... 
+	 */
+	public void startSendingAnnouncements() {
+		Log.info(String.format("Starting Discovery announcements on: %s for: %s -> %s\n", addr, serviceName, serviceURI));
+		
+		byte[] announceBytes = String.format("%s%s%s", serviceName, DELIMITER, serviceURI).getBytes();
+		DatagramPacket announcePkt = new DatagramPacket(announceBytes, announceBytes.length, addr);
+
+		try {
+			if(ms == null) {
+				ms = new MulticastSocket(addr.getPort());
+				ms.joinGroup(addr, NetworkInterface.getByInetAddress(InetAddress.getLocalHost()));
+			}
+
+			// start thread to send periodic announcements
+			new Thread(() -> {
+				for (;;) {
+					try {
+						ms.send(announcePkt);
+						Thread.sleep(DISCOVERY_PERIOD);
+					} catch (Exception e) {
+						e.printStackTrace();
+						// do nothing
+					}
+				}
+			}).start();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * Starts collecting service announcements at regular intervals...
+	 */
+	public void startCollectingAnnouncements() {
+		try {
+			if(ms == null) {
+				ms = new MulticastSocket(addr.getPort());
+				ms.joinGroup(addr, NetworkInterface.getByInetAddress(InetAddress.getLocalHost()));
+			}
+
+			// start thread to collect announcements
+			new Thread(() -> {
+				DatagramPacket pkt = new DatagramPacket(new byte[1024], 1024);
+				for (;;) {
+					try {
+						pkt.setLength(1024);
+						ms.receive(pkt);
+
+						String msg = new String( pkt.getData(), 0, pkt.getLength());
+						String[] msgElems = msg.split(DELIMITER);
+
+						if( msgElems.length == 2) {	//periodic announcement
+							System.out.printf( "FROM %s (%s) : %s\n", pkt.getAddress().getCanonicalHostName(),
+									pkt.getAddress().getHostAddress(), msg);
+
+							String sn = msgElems[0], su = msgElems[1];
+
+							if (!servers.containsKey(sn))
+								servers.put(sn, new HashSet<URI>());
+
+							servers.get(sn).add(URI.create(su));
+							timeStamps.put(sn, System.currentTimeMillis());
+						}
+					} catch (IOException e) {
+						// do nothing
+					}
+				}
+			}).start();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * Returns the known servers for a service.
+	 * 
+	 * @param  serviceName the name of the service being discovered
+	 * @return an array of URI with the service instances discovered. 
+	 * 
+	 */
+	public Set<URI> knownUrisOf(String serviceName) {
+		try {
+			Thread.sleep(DISCOVERY_TIMEOUT);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+
+		return servers.get(serviceName);
+	}	
+	
+	// Main just for testing purposes
+	public static void main( String[] args) throws Exception {
+		Discovery discovery = new Discovery( DISCOVERY_ADDR, "test", "http://" + InetAddress.getLocalHost().getHostAddress());
+		discovery.startSendingAnnouncements();
+		discovery.startCollectingAnnouncements();
+	}
+}
